0abacd78e5c92790a5541655a3ebc20808c7bee8
InfyGrid <infygridsolutions@gmail.com>
2025-11-20 22:48:09 +0530

    Create Terms.jsx

---
+++ frontend/src/pages/ecommerce/Terms.jsx
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <div style={{ padding: "20px", lineHeight: "1.6" }}>
+      <h1>Terms & Conditions</h1>
+      <p>... paste your T&C text here ...</p>
+    </div>
+  );
+}
